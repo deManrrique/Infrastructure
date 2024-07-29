1e93a57a13d1fd3caf951b6d07bc2746d3f05103
DeDuran <98061153+deManrrique@users.noreply.github.com>
2024-07-29 16:20:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "mi-bucket-ejemplo-unique-name"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
